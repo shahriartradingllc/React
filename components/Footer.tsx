f634b7b9350e1850368f03460d00fcc46c92809d
shahriartradingllc <shahriartradingllc@gmail.com>
2025-08-17 15:37:38 +0600

    Delete components directory

--- components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-"use client"
-
-export default function Footer() {
-  return (
-    <footer className="w-full bg-gray-900 text-gray-300 text-center p-4 mt-10">
-      <p>© {new Date().getFullYear()} Shahriyar Trading LLC. All rights reserved.</p>
-    </footer>
-  )
-}
